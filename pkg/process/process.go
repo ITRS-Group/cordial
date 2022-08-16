8f886bc17029836cdcd7f415eb3e649a8b868d28
Peter Galbavy <pgalbavy@itrsgroup.com>
2022-08-16 11:34:55 +0000

    add process package * add Daemon() function

---
+++ pkg/process/process.go
@@ -0,0 +1,73 @@
+package process
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+// Daemon backgrounds the current process by re-executing the existing
+// binary (as found by [os.Executable], so may there is a small window
+// while the referenced binary can change). The function passed as
+// processArgs is called with any further arguments passed to it as
+// parameters and can be used to remove flags that triggered the
+// daemonization in the first place. A helper function - [RemoveArgs] -
+// is available to do this.
+//
+// If successful the function never returns and the child process PID is
+// written to writepid, which can be io.Discard if not required. On
+// failure the function does return with an error.
+//
+//   process.Daemon(os.Stdout, process.RemoveArgs, "-D", "--daemon")
+//
+func Daemon(writepid io.Writer, processArgs func([]string, ...string) []string, args ...string) (err error) {
+	bin, err := os.Executable()
+	if err != nil {
+		return
+	}
+	var newargs []string
+	if processArgs == nil {
+		newargs = RemoveArgs(os.Args[1:], args...)
+	} else {
+		newargs = processArgs(os.Args[1:], args...)
+	}
+	cmd := exec.Command(bin, newargs...)
+	cmd.Stdin = nil
+	cmd.Stdout = nil
+	cmd.Stderr = nil
+
+	// OS specific (compile time/build constraint) change to cmd
+	prepareCmd(cmd)
+
+	if err = cmd.Start(); err != nil {
+		return
+	}
+	if writepid != nil {
+		fmt.Fprintln(writepid, cmd.Process.Pid)
+	}
+	if cmd.Process != nil {
+		cmd.Process.Release()
+	}
+	os.Exit(0)
+	return // not reached
+}
+
+// RemoveArgs is a helper function for Daemon(). Daemon calls the
+// function with os.Args[1;] as origargs and removes any arguments
+// matching members of the slice remove and returns newargs. Only bare
+// arguments are removed and no pattern matching or adjacent values are
+// removed. If this is required then pass your own function with the
+// same signature.
+func RemoveArgs(in []string, remove ...string) (out []string) {
+OUTER:
+	for _, a := range in {
+		for _, r := range remove {
+			if a == r {
+				continue OUTER
+			}
+		}
+		out = append(out, a)
+	}
+	return
+}
