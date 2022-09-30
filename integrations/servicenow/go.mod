1570aec50a23c7a420c8d5510b833e58393ea30b
Peter Galbavy <pgalbavy@itrsgroup.com>
2022-09-30 14:47:24 +0100

    start merging servicenow integration into one binary * very much work in progress

--- integrations/servicenow/go.mod
+++ integrations/servicenow/go.mod
@@ -1,14 +1,17 @@
 module github.com/itrs-group/cordial/integrations/servicenow
 
-replace github.com/itrs-group/cordial => ../../
+go 1.19
 
-go 1.17
+// uncomment the below for debugging changes to cordial locally
+replace github.com/itrs-group/cordial => ../..
 
 require (
-	github.com/itrs-group/cordial v1.0.0
+	github.com/itrs-group/cordial v1.2.1-rc1
 	github.com/labstack/echo/v4 v4.9.0
+	github.com/spf13/cobra v1.5.0
 	github.com/spf13/pflag v1.0.5
-	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1
+	github.com/spf13/viper v1.13.0
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
 )
 
 require (
@@ -16,27 +19,27 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
-	github.com/spf13/afero v1.9.2 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/viper v1.13.0 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
-	golang.org/x/net v0.0.0-20220927171203-f486391704dc // indirect
-	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ integrations/servicenow/snow/lookup_incident.go
@@ -0,0 +1,119 @@
+/*
+Copyright © 2022 ITRS Group
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package snow
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/itrs-group/cordial/pkg/config"
+	"github.com/labstack/echo/v4"
+)
+
+func LookupIncident(vc *config.Config, cmdb_ci_id string, correlation_id string) (incident_id string, short_description string, state int, err error) {
+	s := InitializeConnection(vc)
+
+	q := fmt.Sprintf("active=true^cmdb_ci=%s^correlation_id=%s", cmdb_ci_id, correlation_id)
+	results, err := s.GET("", "sys_id,short_description,state", "", q, "").QueryTableDetail(vc.GetString("servicenow.incidenttable"))
+	if err != nil {
+		err = echo.NewHTTPError(http.StatusInternalServerError, fmt.Sprintf("lookup incident: %s", err))
+		return
+	}
+	if len(results) == 0 {
+		return
+	}
+	incident_id = results["sys_id"]
+	short_description = results["short_description"]
+	state, _ = strconv.Atoi(results["state"])
+	return
+}
+
+func LookupSysIDSimple(vc *config.Config, table, search, cmdb_ci_default string) (sys_id, sys_class_name string, err error) {
+	var field, value string
+	var ok bool
+
+	s := InitializeConnection(vc)
+
+	s1 := strings.SplitN(search, ":", 2)
+	if len(s1) > 1 {
+		table = s1[0]
+		s1 = s1[1:]
+	}
+	s2 := strings.SplitN(s1[0], "=", 2)
+	if len(s2) < 2 {
+		err = echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("invalid simple search %q - must be KEY=VALUE format", s1[0]))
+		return
+	}
+	field = s2[0]
+	value = s2[1]
+
+	var r ResultDetail
+
+	if r, err = s.GET("", "name,sys_id,sys_class_name", "", field+"="+value, "").QueryTableDetail(table); err != nil {
+		err = echo.NewHTTPError(http.StatusNotFound, err)
+		return
+	}
+
+	if sys_id, ok = r["sys_id"]; !ok {
+		if cmdb_ci_default == "" {
+			err = echo.NewHTTPError(http.StatusNotFound, fmt.Sprintf("search %q returned no results", search))
+			return
+		}
+		sys_id = cmdb_ci_default
+	}
+	sys_class_name = r["sys_class_name"]
+
+	return
+}
+
+func LookupSysID(vc *config.Config, table, search, cmdb_ci_default string) (sys_id, sys_class_name string, err error) {
+	var ok bool
+
+	s := InitializeConnection(vc)
+
+	s1 := strings.SplitN(search, ":", 2)
+	if len(s1) == 2 {
+		table = s1[0]
+		search = s1[1]
+	}
+
+	var r ResultDetail
+
+	if r, err = s.GET("", "name,sys_id,sys_class_name", "", search, "").QueryTableDetail(table); err != nil {
+		err = echo.NewHTTPError(http.StatusNotFound, err)
+		return
+	}
+
+	if sys_id, ok = r["sys_id"]; !ok {
+		if cmdb_ci_default == "" {
+			err = echo.NewHTTPError(http.StatusNotFound, nil)
+			return
+		}
+		sys_id = cmdb_ci_default
+	}
+	sys_class_name = r["sys_class_name"]
+
+	return
+}
