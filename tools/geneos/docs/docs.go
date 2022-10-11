8ffb0a0db6f7f8e9f9eec8af2d321b7807a89bfc
Peter Galbavy <pgalbavy@itrsgroup.com>
2022-10-11 09:51:49 +0100

    small updates * add a -V to update to match install * update docs generator * disable flag in root cmd

--- tools/geneos/docs/docs.go
+++ tools/geneos/docs/docs.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"github.com/spf13/cobra/doc"
+	"os"
 
 	"github.com/itrs-group/cordial/tools/geneos/cmd"
+	"github.com/spf13/cobra/doc"
 )
 
+const docdir = "../../../docs/tools/geneos/"
+
 func main() {
-	// doc.GenManTree(cmd.RootCmd(), nil, "./")
-	doc.GenMarkdownTree(cmd.RootCmd(), "./")
+	os.MkdirAll(docdir, 0775)
+	if err := doc.GenMarkdownTree(cmd.RootCmd(), docdir); err != nil {
+		panic(err)
+	}
+
 }
