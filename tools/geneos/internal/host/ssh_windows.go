e26da12a49101778d0bc86bbd628c09af8d1ebe9
Peter Galbavy <pgalbavy@itrsgroup.com>
2022-10-12 14:51:13 +0100

    fix ssh-agent connection on windows

--- tools/geneos/internal/host/ssh_windows.go
+++ tools/geneos/internal/host/ssh_windows.go
@@ -1,10 +1,16 @@
 package host
 
+import (
+	"github.com/Microsoft/go-winio"
+	"github.com/rs/zerolog/log"
+	"golang.org/x/crypto/ssh/agent"
+)
+
 func sshConnectAgent() (agentClient agent.ExtendedAgent) {
 	socket := `\\.\pipe\openssh-ssh-agent`
 	if socket != "" {
 		log.Debug().Msgf("connecting to agent on %s", socket)
-		sshAgent, err := winio.DialPipe("unix", socket)
+		sshAgent, err := winio.DialPipe(socket, nil)
 		if err != nil {
 			log.Error().Msgf("Failed to connect to ssh agent: %v", err)
 		} else {
