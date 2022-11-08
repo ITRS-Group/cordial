19ba63c4ce208ff62497be783cf5d8b01492fbb9
Peter Galbavy <pgalbavy@itrsgroup.com>
2022-11-08 11:00:21 +0000

    small changes, update hocon pkg version

--- tools/sso-diag/cmd/run.go
+++ tools/sso-diag/cmd/run.go
@@ -23,8 +23,6 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -42,6 +40,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
+		start()
 	},
 }
