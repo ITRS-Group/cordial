02446f9041b9b5278d43c1f5f963edd3eaab2131
Peter Galbavy <pgalbavy@itrsgroup.com>
2022-11-22 11:12:16 +0000

    remove raw flag details, add help text

--- tools/geneos/cmd/showHost.go
+++ tools/geneos/cmd/showHost.go
@@ -38,12 +38,8 @@ type showHostCmdConfig struct {
 	Config interface{} `json:"config,omitempty"`
 }
 
-var showHostCmdRaw bool
-
 func init() {
 	showCmd.AddCommand(showHostCmd)
-
-	showHostCmd.Flags().BoolVarP(&showHostCmdRaw, "raw", "r", false, "Show raw (unexpanded) configuration values")
 
 	showHostCmd.Flags().SortFlags = false
 }
@@ -53,6 +49,12 @@ var showHostCmd = &cobra.Command{
 	Use:   "host [flags] [NAME...]",
 	Short: "Show details of remote host configuration",
 	Long: strings.ReplaceAll(`
+Show details of remote host configurations. If no names are supplied
+then all configured hosts are shown.
+
+The output is always unprocessed, and so any values in |expandable|
+format are left as-is. This protects, for example, SSH passwords from
+being accidentally shown in clear text.
 `, "|", "`"),
 	Aliases:      []string{"hosts"},
 	SilenceUsage: true,
