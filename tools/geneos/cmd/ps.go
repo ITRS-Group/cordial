17c7f4e3907a3c0aca3bd56e24f147b138c143aa
Peter Galbavy <pgalbavy@itrsgroup.com>
2023-01-20 07:25:43 +0000

    geneos ps reports incorrect port for Web Servers Fixes #65
    Look in both /proc/net/.tcp and tcp6, ignore filenotexists as tcp6 may not and then ignore hex ip

--- tools/geneos/cmd/ps.go
+++ tools/geneos/cmd/ps.go
@@ -132,7 +132,7 @@ func psInstancePlain(c geneos.Instance, params []string) (err error) {
 		groupname = g.Name
 	}
 	base, underlying, _ := instance.Version(c)
-	ports := instance.TCPPorts(c)
+	ports := instance.TCPListenPorts(c)
 
 	fmt.Fprintf(psTabWriter, "%s\t%s\t%s\t%d\t%v\t%s\t%s\t%s\t%s:%s\t%s\n", c.Type(), c.Name(), c.Host(), pid, ports, username, groupname, time.Unix(mtime, 0).Local().Format(time.RFC3339), base, underlying, c.Home())
 
