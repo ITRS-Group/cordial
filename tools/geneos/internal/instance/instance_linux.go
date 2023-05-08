4999a819776ee8237c99e7a8ce6ceab9191476c1
Peter Galbavy <pgalbavy@itrsgroup.com>
2023-05-08 10:10:33 +0100

    remove function moved to pkg/host

--- tools/geneos/internal/instance/instance_linux.go
+++ tools/geneos/internal/instance/instance_linux.go
@@ -29,13 +29,11 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
-	"os/exec"
 	"path"
 	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/itrs-group/cordial/tools/geneos/internal/geneos"
@@ -265,20 +263,4 @@ func Sockets(c geneos.Instance) (links map[int]int) {
 		}
 	}
 	return
-}
-
-func procSetupOS(cmd *exec.Cmd, out *os.File) {
-	var err error
-
-	// if we've set-up privs at all, set the redirection output file to the same
-	if cmd.SysProcAttr != nil && cmd.SysProcAttr.Credential != nil {
-		if err = out.Chown(int(cmd.SysProcAttr.Credential.Uid), int(cmd.SysProcAttr.Credential.Gid)); err != nil {
-			log.Error().Err(err).Msg("chown")
-		}
-	}
-	// detach process by creating a session (fixed start + log)
-	if cmd.SysProcAttr == nil {
-		cmd.SysProcAttr = &syscall.SysProcAttr{}
-	}
-	cmd.SysProcAttr.Setsid = true
 }
