3e9f2215474d756df4d135b1c61c752195ff0afc
Peter Galbavy <pgalbavy@itrsgroup.com>
2023-05-15 17:54:07 +0100

    deprecate `logger`

--- examples/api/cpu/cpu.go
+++ examples/api/cpu/cpu.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"runtime"
 
-	"github.com/itrs-group/cordial/pkg/logger"
+	"github.com/rs/zerolog/log"
+
 	"github.com/itrs-group/cordial/pkg/plugins"
 	"github.com/itrs-group/cordial/pkg/samplers"
 )
@@ -13,26 +14,20 @@ func init() {
 	// logger.EnableDebugLog()
 }
 
-var (
-	log      = logger.Log
-	logDebug = logger.Debug
-	logError = logger.Error
-)
-
 type CPUSampler struct {
 	samplers.Samplers
 	cpustats cpustat
 }
 
 func New(s *plugins.Connection, name string, group string) (*CPUSampler, error) {
-	logDebug.Println("called")
+	log.Debug().Msg("called")
 	c := new(CPUSampler)
 	c.Plugins = c
 	return c, c.New(s, name, group)
 }
 
 func (p *CPUSampler) InitSampler() (err error) {
-	logDebug.Println("called")
+	log.Debug().Msg("called")
 	p.Headline("OS", runtime.GOOS)
 	p.Headline("SampleInterval", fmt.Sprintf("%v", p.Interval()))
 
