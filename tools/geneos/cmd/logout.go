f2d8079f1d5d38c30b9b245ca27512ecc516281a
Peter Galbavy <pgalbavy@itrsgroup.com>
2023-05-15 12:17:09 +0100

    flesh out logout command

--- tools/geneos/cmd/logout.go
+++ tools/geneos/cmd/logout.go
@@ -4,7 +4,6 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -12,22 +11,37 @@ import (
 	"github.com/itrs-group/cordial/pkg/config"
 )
 
+var logoutCmdAll bool
+
 func init() {
 	RootCmd.AddCommand(logoutCmd)
+
+	logoutCmd.Flags().BoolVarP(&logoutCmdAll, "all", "A", false, "remove all credentials")
 }
 
 // logoutCmd represents the logout command
 var logoutCmd = &cobra.Command{
-	Use:   "logout",
+	Use:   "logout [flags] [NAME...]",
 	Short: "Logout (remove credentials)",
 	Long: strings.ReplaceAll(`
+The logout command removes the credentials for the names given. If no
+names are set then the default credentials are removed.
+
+If the |-A| options is given then all credentials are removed.
 `, "|", "`"),
 	Annotations: map[string]string{
 		"wildcard":     "false",
 		"needshomedir": "false",
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		config.DeleteCreds("x", config.SetAppName(Execname))
-		fmt.Println("logout called")
+		if logoutCmdAll {
+			config.DeleteAllCreds(config.SetAppName(Execname))
+			return
+		}
+		if len(args) == 0 {
+			for _, d := range args {
+				config.DeleteCreds(d, config.SetAppName(Execname))
+			}
+		}
 	},
 }
