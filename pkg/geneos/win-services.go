186a50f5306f9fe52bce9c50545fe9ed4b701cb8
Peter Galbavy <pgalbavy@itrsgroup.com>
2023-06-28 13:07:08 +0100

    add win-services layout

--- pkg/geneos/win-services.go
+++ pkg/geneos/win-services.go
@@ -25,9 +25,27 @@ package geneos
 import "encoding/xml"
 
 type WinServicesPlugin struct {
-	XMLName xml.Name `xml:"win-services" json:"-" yaml:"-"`
+	XMLName     xml.Name              `xml:"win-services" json:"-" yaml:"-"`
+	ServicesOld []SingleLineStringVar `xml:"services,omitempty" json:"servicesOld,omitempty" yaml:"servicesOld,omitempty"`
+	Services    []Service             `xml:"servicesNew>service,omitempty" json:"services,omitempty" yaml:"services,omitempty"`
 }
 
 func (p *WinServicesPlugin) String() string {
 	return p.XMLName.Local
+}
+
+type Service struct {
+	ServiceName        *ServiceName        `xml:"serviceName,omitempty" json:"name,omitempty" yaml:"name,omitempty"`
+	ServiceDescription *ServiceDescription `xml:"serviceDescription,omitempty" json:"description,omitempty" yaml:"description,omitempty"`
+}
+
+type ServiceName struct {
+	Basic                *SingleLineStringVar `xml:"basic>pattern,omitempty" json:"basic,omitempty" yaml:"basic,omitempty"`
+	BasicShowUnavailable bool                 `xml:"basic>showIfUnavailable,omitempty" json:"showIfUnavailable,omitempty" yaml:"showIfUnavailable,omitempty"`
+	Regex                *Regex               `xml:"regex,omitempty" json:"regex,omitempty" yaml:"regex,omitempty"`
+}
+
+type ServiceDescription struct {
+	Basic *SingleLineStringVar `xml:"basic>pattern,omitempty" json:"basic,omitempty" yaml:"basic,omitempty"`
+	Regex *Regex               `xml:"regex,omitempty" json:"regex,omitempty" yaml:"regex,omitempty"`
 }
