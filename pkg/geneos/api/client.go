497863b9429eaae51655e5bcb8ce0d9315dcfc35
Peter Galbavy <pgalbavy@itrsgroup.com>
2023-08-18 15:03:27 +0100

    updates and fixes - still WIP

--- pkg/geneos/api/client.go
+++ pkg/geneos/api/client.go
@@ -7,16 +7,30 @@ import (
 // APIClient is the method set required for any API sending data into a Geneos Netprobe
 type APIClient interface {
 	Healthy() bool
+
 	CreateDataview(entity, sampler, name string) error
 	UpdateDataview(entity, sampler, name string, values [][]string) error
 	DeleteDataview(entity, sampler, name string) error
+
 	CreateRow(entity, sampler, view, name string) error
 	UpdateRow(entity, sampler, view, name string, values []string) error
 	DeleteRow(entity, sampler, view, name string) error
-	CreateStream(entity, sampler, name string) error
-	UpdateStream(entity, sampler, name, message string) error
 
+	CreateColumn(entity, sampler, view, name string) error
+
+	CreateHeadline(entity, sampler, view, name string) error
+	UpdateHeadline(entity, sampler, view, name, value string) error
+	DeleteHeadline(entity, sampler, view, name string) error
+
+	CreateStream(entity, sampler, name string) error
+	UpdateStream(entity, sampler, name string, message any) error
+
+	ManagedEntityExists(entity string) (bool, error)
+	SamplerExists(entity, sampler string) (bool, error)
 	DataviewExists(entity, sampler, name string) (bool, error)
+	ColumnExists(entity, sampler, view, name string) (bool, error)
+	RowExists(entity, sampler, view, name string) (bool, error)
+	HeadlineExists(entity, sampler, view, name string) (bool, error)
 }
 
 type roundTripper struct {
@@ -29,3 +43,28 @@ func (r *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 
 // compile time check for interface validity
 var _ http.RoundTripper = (*roundTripper)(nil)
+
+type Stream struct {
+	client  APIClient
+	entity  string
+	sampler string
+	stream  string
+}
+
+func OpenStream(c APIClient, entity, sampler, stream string) (s *Stream, err error) {
+	s = &Stream{
+		client:  c,
+		entity:  entity,
+		sampler: sampler,
+		stream:  stream,
+	}
+	return
+}
+
+func (s *Stream) Write(data []byte) (n int, err error) {
+	if err = s.client.UpdateStream(s.entity, s.sampler, s.stream, string(data)); err != nil {
+		return
+	}
+	n = len(data)
+	return
+}
--- pkg/geneos/api/rest.go
+++ pkg/geneos/api/rest.go
@@ -3,6 +3,7 @@ package api
 import (
 	"context"
 	"errors"
+	"net/url"
 
 	"github.com/itrs-group/cordial/pkg/rest"
 )
@@ -26,11 +27,12 @@ type RESTClient struct {
 var _ APIClient = (*RESTClient)(nil)
 
 func NewRESTClient(endpoint string, options ...rest.Options) (c APIClient, err error) {
+	options = append(options, rest.BaseURL(endpoint))
 	return &RESTClient{Client: rest.NewClient(options...)}, nil
 }
 
 func (c *RESTClient) Healthy() bool {
-	resp, err := c.Get(context.Background(), "/liveness", nil, nil)
+	resp, err := c.Get(context.Background(), "/healthcheck", nil, nil)
 	if err != nil || resp.StatusCode != 200 {
 		return false
 	}
@@ -61,14 +63,54 @@ func (c *RESTClient) DeleteRow(entity, sampler, view, name string) (err error) {
 	return
 }
 
-func (c *RESTClient) CreateStream(entity, sampler, name string) (err error) {
+func (c *RESTClient) CreateColumn(entity, sampler, view, name string) (err error) {
 	return
 }
 
-func (c *RESTClient) UpdateStream(entity, sampler, name, message string) (err error) {
+func (c *RESTClient) CreateHeadline(entity, sampler, view, name string) (err error) {
 	return
+}
+
+func (c *RESTClient) UpdateHeadline(entity, sampler, view, name, value string) (err error) {
+	return
+}
+
+func (c *RESTClient) DeleteHeadline(entity, sampler, view, name string) (err error) {
+	return
+}
+
+func (c *RESTClient) CreateStream(entity, sampler, name string) (err error) {
+	endpoint, _ := url.JoinPath("managedEntity", entity, "sampler", sampler, "stream", name)
+	_, err = c.Put(context.Background(), endpoint, nil, nil)
+	return
+}
+
+func (c *RESTClient) UpdateStream(entity, sampler, name string, message any) (err error) {
+	endpoint, _ := url.JoinPath("managedEntity", entity, "sampler", sampler, "stream", name)
+	_, err = c.Put(context.Background(), endpoint, message, nil)
+	return
+}
+
+func (c *RESTClient) ManagedEntityExists(entity string) (bool, error) {
+	return false, errors.ErrUnsupported
+}
+
+func (c *RESTClient) SamplerExists(entity, sampler string) (bool, error) {
+	return false, errors.ErrUnsupported
 }
 
 func (c *RESTClient) DataviewExists(entity, sampler, name string) (bool, error) {
+	return false, errors.ErrUnsupported
+}
+
+func (c *RESTClient) RowExists(entity, sampler, view, name string) (bool, error) {
+	return false, errors.ErrUnsupported
+}
+
+func (c *RESTClient) ColumnExists(entity, sampler, view, name string) (bool, error) {
+	return false, errors.ErrUnsupported
+}
+
+func (c *RESTClient) HeadlineExists(entity, sampler, view, name string) (bool, error) {
 	return false, errors.ErrUnsupported
 }
