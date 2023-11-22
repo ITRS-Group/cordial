59035b668f43c01971cc463ebd0830cbb4686927
Peter Galbavy <pgalbavy@itrsgroup.com>
2023-11-22 14:51:26 +0000

    add support for app key generation

--- tools/geneos/cmd/aescmd/_docs/encode.md
+++ tools/geneos/cmd/aescmd/_docs/encode.md
@@ -1,8 +1,10 @@
-Encode plaintext to a Geneos AES256 format password using a key file.
+Encode plaintext to a Geneos AES256 format password using a key file, or create a Gateway "app key" file.
 
-A key file must either be provided using the `-k` option or otherwise all matching instances that have a configured key file are used to produce an encrypted password.
+A key file should be provided using the `-keyfile`/`-k` option for a file path, the `--crc`/`-c` option for the CRC of a shared keyfile, or otherwise all matching instances that have a configured key file are used to produce an encrypted password.
 
-The plaintext password can be provided in three ways.
+## Encoding Passwords
+
+For encoding passwords, the plaintext password can be provided in one three ways:
 
 1. The default is to prompt for the plaintext and again to verify they match.
 2. Alternatively the password can be provided directly on the command line using the `-p plaintext` flag or,
@@ -19,3 +21,13 @@ Rather than:
 ```bash
 $ echo -n "test" | geneos aes encode -s -
 ```
+
+## App Keys
+
+To create an app key file suitable for connecting to an SSO Agent, Gateway Hub or Obcerv from a Gateway for Centralised Configuration basic authentication support use the `--app-key`/`-A` flag. The value passed with the flag must be a valid provider, which is one of: "`ssoAgent`", "`gatewayHub`" or "`obcerv`". These values are case-sensitive.
+
+The client ID and client secret can either be passed on the command line using the `--client-id`/`-C` and `--client-secret`/`-S` flags respectively, or you will be prompted to enter one or both using a non-echoing password-like dialogue.
+
+The app key file contents are written to STDOUT unless you supply an `-app-key-file`/`-a` filename. This should be a file name and not a file path, and will be used to write an app key file in each matching instance home directory. If you supply a file path then the results are undetermined.
+
+The contents of the app key output of saved file should be identical to that of the `-store-app-key` Gateway command line option but using an external key file (only).
--- tools/geneos/cmd/aescmd/encode.go
+++ tools/geneos/cmd/aescmd/encode.go
@@ -24,9 +24,11 @@ package aescmd
 
 import (
 	_ "embed"
+	"errors"
 	"fmt"
 	"os"
 
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 
 	"github.com/itrs-group/cordial/pkg/config"
@@ -36,8 +38,8 @@ import (
 )
 
 var encodeCmdKeyfile config.KeyFile
-var encodeCmdString *config.Plaintext
-var encodeCmdSource string
+var encodeCmdString, encodeCmdClientID, encodeCmdClientSecret *config.Plaintext
+var encodeCmdSource, encodeCmdProvider, encodeCmdAppKeyFile, encodeCmdCRC string
 var encodeCmdExpandable, encodeCmdAskOnce bool
 
 func init() {
@@ -47,9 +49,18 @@ func init() {
 
 	encodeCmd.Flags().BoolVarP(&encodeCmdExpandable, "expandable", "e", false, "Output in 'expandable' format")
 	encodeCmd.Flags().VarP(&encodeCmdKeyfile, "keyfile", "k", "Path to keyfile")
+	encodeCmd.Flags().StringVarP(&encodeCmdCRC, "crc", "c", "", "CRC of existing component shared keyfile to use (extension optional)")
+
 	encodeCmd.Flags().VarP(encodeCmdString, "password", "p", "Plaintext password")
 	encodeCmd.Flags().StringVarP(&encodeCmdSource, "source", "s", "", "Alternative source for plaintext password")
 	encodeCmd.Flags().BoolVarP(&encodeCmdAskOnce, "once", "o", false, "Only prompt for password once, do not verify. Normally use '-s -' for stdin")
+
+	encodeCmdClientID = &config.Plaintext{}
+	encodeCmdClientSecret = &config.Plaintext{}
+	encodeCmd.Flags().StringVarP(&encodeCmdProvider, "app-key", "A", "", "SSO `PROVIDER`, one of ssoAgent, obcerv, gatewayHub")
+	encodeCmd.Flags().VarP(encodeCmdClientID, "client-id", "C", "Client ID for --app-key, prompted if not set")
+	encodeCmd.Flags().VarP(encodeCmdClientSecret, "client-secret", "S", "Client Secret for --app-key, prompted if not set")
+	encodeCmd.Flags().StringVarP(&encodeCmdAppKeyFile, "app-key-file", "a", "", "App-key filename, if saving per instance, otherwise defaults to STDOUT")
 
 	encodeCmd.Flags().SortFlags = false
 }
@@ -70,6 +81,101 @@ var encodeCmd = &cobra.Command{
 		cmd.AnnotationExpand:    "true",
 	},
 	RunE: func(command *cobra.Command, _ []string) (err error) {
+		h := geneos.GetHost(cmd.Hostname)
+
+		if encodeCmdProvider != "" {
+			if encodeCmdProvider != "ssoAgent" && encodeCmdProvider != "obcerv" && encodeCmdProvider != "gatewayHub" {
+				return errors.New("--app-key PROVIDER must be one of `obcerv`, `ssoAgent` or `gatewayHub`")
+			}
+			if encodeCmdClientID.IsNil() {
+				encodeCmdClientID, err = config.ReadPasswordInput(false, 3, "Client ID")
+				if err != nil {
+					return
+				}
+			}
+			if encodeCmdClientSecret.IsNil() {
+				encodeCmdClientSecret, err = config.ReadPasswordInput(false, 3, "Client Secret")
+				if err != nil {
+					return
+				}
+			}
+			ct, args := cmd.ParseTypeNames(command)
+
+			if encodeCmdAppKeyFile == "" {
+				// to stdout
+				keyfilepath, _ := ct.KeyFilePath(h, encodeCmdKeyfile, encodeCmdCRC)
+
+				log.Debug().Msgf("keyfilepath=%s", keyfilepath)
+
+				var keyfile config.KeyFile
+				keyfile = config.KeyFile(keyfilepath)
+				if keyfile == "" {
+					return
+				}
+
+				e, err := keyfile.Encode(encodeCmdClientSecret, false)
+				if err != nil {
+					return err
+				}
+				fmt.Println("# Gateway client secret. Please ensure that this file has restricted access")
+				fmt.Printf("client_id: %s\n", encodeCmdClientID)
+				fmt.Printf("client_secret: %s\n", e)
+				fmt.Printf("sso_provider: %s\n", encodeCmdProvider)
+				return nil
+			}
+
+			if ct == nil {
+				ct = geneos.ParseComponent("gateway")
+			}
+			if !ct.IsA("gateway") {
+				return fmt.Errorf("app keys are only valid for gateways")
+			}
+
+			instance.Do(h, ct, args, func(i geneos.Instance, params ...any) (resp *instance.Response) {
+				resp = instance.NewResponse(i)
+				if !i.Type().UsesKeyfiles {
+					return
+				}
+
+				keyfilepath, _ := i.Type().KeyFilePath(i.Host(), encodeCmdKeyfile, encodeCmdCRC)
+
+				log.Debug().Msgf("keyfilepath=%s", keyfilepath)
+
+				var keyfile config.KeyFile
+				keyfile = config.KeyFile(keyfilepath)
+				if keyfile == "" {
+					keyfile = config.KeyFile(instance.PathOf(i, "keyfile"))
+					if keyfile == "" {
+						return
+					}
+				}
+
+				e, err := keyfile.Encode(encodeCmdClientSecret, false)
+				if err != nil {
+					resp.Err = err
+					return
+				}
+
+				dest := instance.Abs(i, encodeCmdAppKeyFile)
+				w, err := i.Host().Create(dest, 0640)
+				if err != nil {
+					resp.Err = fmt.Errorf("%w create(%s)", err, dest)
+					return
+				}
+
+				fmt.Fprintln(w, "# Gateway client secret. Please ensure that this file has restricted access")
+				fmt.Fprintf(w, "client_id: %s\n", encodeCmdClientID)
+				fmt.Fprintf(w, "client_secret: %s\n", e)
+				fmt.Fprintf(w, "sso_provider: %s\n", encodeCmdProvider)
+				w.Close()
+
+				resp.Completed = append(resp.Completed, fmt.Sprintf("app key written to %s", dest))
+
+				return
+			}).Write(os.Stdout)
+			return
+		}
+
 		var plaintext *config.Plaintext
 		if !encodeCmdString.IsNil() {
 			plaintext = encodeCmdString
@@ -86,18 +192,28 @@ var encodeCmd = &cobra.Command{
 			}
 		}
 
-		if encodeCmdKeyfile != "" {
-			// encode using specific file
-			e, err := encodeCmdKeyfile.Encode(plaintext, encodeCmdExpandable)
-			if err != nil {
-				return err
+		ct, args := cmd.ParseTypeNames(command)
+
+		if encodeCmdKeyfile != "" || encodeCmdCRC != "" {
+			for _, h := range h.OrList(geneos.ALL) {
+				for _, ct := range ct.OrList(geneos.UsesKeyFiles()...) {
+					keyfilepath, _ := ct.KeyFilePath(h, encodeCmdKeyfile, encodeCmdCRC)
+					if keyfilepath != "" {
+						keyfile := config.KeyFile(keyfilepath)
+						// encode using specific file
+						e, err := keyfile.Encode(plaintext, encodeCmdExpandable)
+						if err != nil {
+							continue
+						}
+						fmt.Println(e)
+						return nil
+					}
+				}
 			}
-			fmt.Println(e)
-			return nil
+			return fmt.Errorf("no matching keyfile found")
 		}
 
-		ct, args := cmd.ParseTypeNames(command)
-		instance.Do(geneos.GetHost(cmd.Hostname), ct, args, func(i geneos.Instance, params ...any) (resp *instance.Response) {
+		instance.Do(h, ct, args, func(i geneos.Instance, params ...any) (resp *instance.Response) {
 			resp = instance.NewResponse(i)
 
 			if len(params) == 0 {
