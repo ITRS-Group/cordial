1eca20eb81e5a3ccafc345d9c77430fd6425b40f
Peter Galbavy <pgalbavy@itrsgroup.com>
2024-05-24 10:45:00 +0100

    `tls create` updates: * add --dest for directory to write files * add option to output a --bundle (optionally to stdout via new --dest "-") * add --days for expiry period * only initialise the tls subsystem if the --force flag is given * update docs

--- tools/geneos/cmd/tlscmd/create.go
+++ tools/geneos/cmd/tlscmd/create.go
@@ -27,6 +27,7 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	_ "embed"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"os"
@@ -42,16 +43,22 @@ import (
 	"github.com/itrs-group/cordial/tools/geneos/internal/geneos"
 )
 
-var createCmdCN string
-var createCmdOverwrite bool
+var createCmdCN, createCmdDest string
+var createCmdOverwrite, createCmdBundle bool
 var createCmdSANs createCmdSAN
+var createCmdDays int
 
 func init() {
 	tlsCmd.AddCommand(createCmd)
 
+	createCmd.Flags().BoolVarP(&createCmdBundle, "bundle", "b", false, "Create a certificate bundle, including certificate, key and verification chain")
+	createCmd.Flags().StringVar(&createCmdDest, "dest", ".", "Destination to write results to, use a dash '-' for console for bundles only")
+
 	createCmd.Flags().StringVarP(&createCmdCN, "cname", "c", "", "Common Name for certificate. Defaults to hostname")
 	createCmd.Flags().VarP(&createCmdSANs, "san", "s", "Subject-Alternative-Name (repeat for each one required). Defaults to hostname if none given")
-	createCmd.Flags().BoolVarP(&createCmdOverwrite, "force", "F", false, "Force overwrite existing certificate (but not root and intermediate)")
+	createCmd.Flags().BoolVarP(&createCmdOverwrite, "force", "F", false, "Run `tls init` and force overwrite any existing file in 'dest'")
+	createCmd.Flags().IntVarP(&createCmdDays, "days", "D", 365, "Certificate duration in days")
+
 }
 
 //go:embed _docs/create.md
@@ -71,11 +78,34 @@ var createCmd = &cobra.Command{
 			hostname, _ := os.Hostname()
 			createCmdSANs = []string{hostname}
 		}
-		geneos.TLSInit(false, initCmdKeyType)
+		if createCmdOverwrite {
+			if err = geneos.TLSInit(false, initCmdKeyType); err != nil {
+				return
+			}
+		}
 		if createCmdCN == "" {
 			createCmdCN, _ = os.Hostname()
 		}
-		err = CreateCert(".", createCmdOverwrite, createCmdCN, createCmdSANs...)
+		if createCmdBundle {
+			err = CreateCertBundle(createCmdDest, createCmdOverwrite, 24*time.Hour*time.Duration(createCmdDays), createCmdCN, createCmdSANs...)
+			if err != nil {
+				if errors.Is(err, os.ErrExist) && !createCmdOverwrite {
+					fmt.Printf("Certificate file already exists for CN=%q, use --force to overwrite\n", createCmdCN)
+					return nil
+				}
+				return
+			}
+			if createCmdDest != "-" {
+				fmt.Printf("Certificate bundle created for CN=%q\n", createCmdCN)
+			}
+			return
+		}
+
+		if createCmdDest == "-" {
+			fmt.Println("Console output only valid for bundles")
+			return nil
+		}
+		err = CreateCert(createCmdDest, createCmdOverwrite, 24*time.Hour*time.Duration(createCmdDays), createCmdCN, createCmdSANs...)
 		if err != nil {
 			if errors.Is(err, os.ErrExist) && !createCmdOverwrite {
 				fmt.Printf("Certificate already exists for CN=%q, use --force to overwrite\n", createCmdCN)
@@ -88,13 +118,11 @@ var createCmd = &cobra.Command{
 	},
 }
 
-// CreateCert creates a new certificate
-//
-// this also creates a new private key
+// CreateCert creates a new certificate and private key
 //
 // skip if certificate exists and is valid
-func CreateCert(dir string, overwrite bool, cn string, san ...string) (err error) {
-	basepath := path.Join(dir, strings.ReplaceAll(cn, " ", "-"))
+func CreateCert(destination string, overwrite bool, duration time.Duration, cn string, san ...string) (err error) {
+	basepath := path.Join(destination, strings.ReplaceAll(cn, " ", "-"))
 	if _, err = os.Stat(basepath + ".pem"); err == nil && !overwrite {
 		return os.ErrExist
 	}
@@ -102,7 +130,10 @@ func CreateCert(dir string, overwrite bool, cn string, san ...string) (err error
 	if err != nil {
 		return
 	}
-	expires := time.Now().AddDate(1, 0, 0).Truncate(24 * time.Hour)
+	if duration == 0 {
+		duration = 365 * 24 * time.Hour
+	}
+	expires := time.Now().Add(duration)
 	template := x509.Certificate{
 		SerialNumber: serial,
 		Subject: pkix.Name{
@@ -142,6 +173,79 @@ func CreateCert(dir string, overwrite bool, cn string, san ...string) (err error
 	}
 
 	fmt.Printf("certificate created for %s (expires %s)\n", basepath, expires.UTC())
+
+	return
+}
+
+func CreateCertBundle(destination string, overwrite bool, duration time.Duration, cn string, san ...string) (err error) {
+	basepath := path.Join(destination, strings.ReplaceAll(cn, " ", "-"))
+	if destination != "-" {
+		if _, err = os.Stat(basepath + ".pem"); err == nil && !overwrite {
+			return os.ErrExist
+		}
+	}
+	serial, err := rand.Prime(rand.Reader, 64)
+	if err != nil {
+		return
+	}
+	if duration == 0 {
+		duration = 365 * 24 * time.Hour
+	}
+	expires := time.Now().Add(duration)
+	template := x509.Certificate{
+		SerialNumber: serial,
+		Subject: pkix.Name{
+			CommonName: cn,
+		},
+		NotBefore:      time.Now().Add(-60 * time.Second),
+		NotAfter:       expires,
+		KeyUsage:       x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:    []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		MaxPathLenZero: true,
+		DNSNames:       san,
+		// IPAddresses:    []net.IP{net.ParseIP("127.0.0.1")},
+	}
+
+	root, rootFile, err := geneos.ReadRootCert(true)
+	if err != nil {
+		err = fmt.Errorf("local root certificate (%s) not valid: %w", rootFile, err)
+		return
+	}
+	signer, signerFile, err := geneos.ReadSigningCert(true)
+	if err != nil {
+		err = fmt.Errorf("local signing root certificate (%s) not valid: %w", signerFile, err)
+		return
+	}
+	signingKey, err := config.ReadPrivateKey(geneos.LOCAL, path.Join(config.AppConfigDir(), geneos.SigningCertFile+".key"))
+	if err != nil {
+		return
+	}
+
+	cert, key, err := config.CreateCertificateAndKey(&template, signer, signingKey, nil)
+	if err != nil {
+		return
+	}
+
+	var pembytes []byte
+	for _, c := range []*x509.Certificate{cert, signer, root} {
+		pembytes = append(pembytes, pem.EncodeToMemory(&pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: c.Raw,
+		})...)
+	}
+
+	l, _ := key.Open()
+	pembytes = append(pembytes, pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: l.Bytes(),
+	})...)
+	l.Destroy()
+
+	if destination != "-" {
+		return os.WriteFile(basepath+".pem", pembytes, 0600)
+	}
+
+	fmt.Println(string(pembytes))
 
 	return
 }
