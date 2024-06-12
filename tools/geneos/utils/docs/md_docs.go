1d4e7d104b436705bdab019e2c82d0e5a2421525
Peter Galbavy <pgalbavy@itrsgroup.com>
2024-06-12 08:22:33 +0100

    move to Apache 2 license

--- tools/geneos/utils/docs/md_docs.go
+++ tools/geneos/utils/docs/md_docs.go
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 // This is a customised copy of the md_docs.go source from the cobra
 // project but with change to meet local cordial requirements.
 
