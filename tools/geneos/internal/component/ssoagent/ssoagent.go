f0639acf2de549f12432469404ae51e4b00b943d
Peter Galbavy <pgalbavy@itrsgroup.com>
2024-10-31 11:09:41 +0000

    tools/geneos: sso-agent - build keystore correctly, add an "ssokey" if one does exist

--- tools/geneos/internal/component/ssoagent/ssoagent.go
+++ tools/geneos/internal/component/ssoagent/ssoagent.go
@@ -18,15 +18,21 @@ limitations under the License.
 package ssoagent
 
 import (
+	"crypto/rand"
 	"crypto/x509"
+	"crypto/x509/pkix"
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"path"
 	"regexp"
+	"slices"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/awnumar/memguard"
 	"github.com/pavlo-v-chernykh/keystore-go/v4"
 	"github.com/rs/zerolog/log"
 
@@ -221,7 +227,8 @@ func (s *SSOAgents) Add(tmpl string, port uint16) (err error) {
 	}
 
 	for _, source := range ssoagentFiles {
-		if _, err = geneos.ImportFile(s.Host(), s.Home(), source); err != nil && err != geneos.ErrExists {
+		if _, err = geneos.ImportFile(s.Host(), s.Home(), source); err != nil && !errors.Is(err, geneos.ErrExists) {
+			log.Warn().Err(err).Msgf("source file %q may not exist", source)
 			return
 		}
 	}
@@ -231,52 +238,49 @@ func (s *SSOAgents) Add(tmpl string, port uint16) (err error) {
 }
 
 func (s *SSOAgents) Rebuild(initial bool) (err error) {
-	// load the security.properties file, update the port and use the keystor values later
-	sp, err := instance.ReadKVConfig(s.Host(), path.Join(s.Home(), "config/security.properties"))
-	if err != nil {
-		return nil
-	}
-	sp["port"] = s.Config().GetString("port")
-	if err = instance.WriteKVConfig(s.Host(), path.Join(s.Home(), "config/security.properties"), sp); err != nil {
-		panic(err)
+	cf := s.Config()
+	ssoconf := config.New()
+	if err = ssoconf.MergeHOCONFile(path.Join(s.Home(), "conf/sso-agent.conf")); err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return nil
+		}
+		return err
 	}
 
-	// rebuild the truststore (local cacerts) if we have a `truststore`
-	// and `certchain` defined. This is used for connection *to* other
-	// components, such as secure gateways and SSO agent.
-	cf := s.Config()
-	if cf.IsSet("truststore") && cf.IsSet("certchain") {
-		log.Debug().Msgf("%s: rebuilding truststore: %q", s.String(), cf.GetString("truststore"))
+	if ssoconf.IsSet(config.Join("server", "trust_store", "location")) && cf.IsSet("certchain") {
+		trustStore := instance.Abs(s, ssoconf.GetString(config.Join("server", "trust_store", "location")))
+		trustStorePassword := ssoconf.GetPassword(config.Join("server", "trust_store", "password"), config.Default("changeit"))
+		log.Debug().Msgf("%s: rebuilding truststore: %q", s.String(), trustStore)
 		certs := config.ReadCertificates(s.Host(), cf.GetString("certchain"))
 		k, err := geneos.ReadKeystore(s.Host(),
-			cf.GetString("truststore"),
-			cf.GetPassword("truststore-password", config.Default("changeit")),
+			trustStore,
+			trustStorePassword,
 		)
-		if err != nil {
-			return err
-		}
-		for _, cert := range certs {
-			alias := cert.Subject.CommonName
-			log.Debug().Msgf("%s: replacing entry for %q", s.String(), alias)
-			k.DeleteEntry(alias)
-			if err = k.AddKeystoreCert(alias, cert); err != nil {
-				return err
+		// if trust exists, check for existing cert
+		if err == nil {
+			for _, cert := range certs {
+				alias := cert.Subject.CommonName
+				log.Debug().Msgf("%s: replacing entry for %q", s.String(), alias)
+				k.DeleteEntry(alias)
+				if err = k.AddKeystoreCert(alias, cert); err != nil {
+					return err
+				}
 			}
 		}
+
 		// TODO: temp file dance, after testing
-		log.Debug().Msgf("%s: writing new truststore to %q", s.String(), cf.GetString("truststore"))
-		if err = k.WriteKeystore(s.Host(),
-			cf.GetString("truststore"),
-			cf.GetPassword("truststore-password", config.Default("changeit")),
-		); err != nil {
+		log.Debug().Msgf("%s: writing new truststore to %q", s.String(), trustStore)
+		if err = k.WriteKeystore(s.Host(), trustStore, trustStorePassword); err != nil {
 			return err
 		}
 	}
 
-	// rebuild the keystore (config/ketstore.db) is certificate and
-	// privatekey are defined. This is for client connections to the web
-	// dashboard and will typically be a "real" certificate.
-	if cf.IsSet("certificate") && cf.IsSet("privatekey") {
+	// rebuild the keystore (config/keystore.db) is certificate and
+	// privatekey are defined. This is for client connections to the
+	// sso-agent and will typically be a "real" certificate.
+	if ssoconf.IsSet(config.Join("server", "key_store", "location")) && cf.IsSet("privatekey") {
+		keyStore := instance.Abs(s, ssoconf.GetString(config.Join("server", "key_store", "location")))
+		log.Debug().Msgf("%s: rebuilding keystore: %q", s.String(), keyStore)
 		cert, err := config.ParseCertificate(s.Host(), cf.GetString("certificate"))
 		if err != nil {
 			return err
@@ -289,27 +293,56 @@ func (s *SSOAgents) Rebuild(initial bool) (err error) {
 		if cf.IsSet("certchain") {
 			chain = append(chain, config.ReadCertificates(s.Host(), cf.GetString("certchain"))...)
 		}
-		keyStore, ok := sp["keyStore"]
-		if !ok {
-			return fmt.Errorf("keyStore not defined in security.properties")
-		}
-		if _, ok = sp["keyStorePassword"]; !ok {
-			return fmt.Errorf("keyStorePassword not defined in security.properties")
-		}
-		keyStorePassword := cf.ExpandToPassword(sp["keyStorePassword"])
-		k, err := geneos.ReadKeystore(s.Host(), path.Join(s.Home(), keyStore), keyStorePassword)
+		keyStorePassword := ssoconf.GetPassword(config.Join("server", "key_store", "password"), config.Default("changeit"))
+		k, err := geneos.ReadKeystore(s.Host(), keyStore, keyStorePassword)
 		if err != nil {
 			// new, empty keystore
 			k = geneos.KeyStore{
 				KeyStore: keystore.New(),
 			}
 		}
+		if !slices.Contains(k.Aliases(), "ssokey") {
+			cert, key, err := genkeypair()
+			if err != nil {
+				log.Fatal().Err(err).Msg("")
+			}
+			chain := []*x509.Certificate{cert}
+			if err = k.AddKeystoreKey("ssokey", key, keyStorePassword, chain); err != nil {
+				log.Fatal().Err(err).Msg("")
+			}
+		}
 		alias := geneos.ALL.Hostname()
 		k.DeleteEntry(alias)
 		k.AddKeystoreKey(alias, key, keyStorePassword, chain)
-		return k.WriteKeystore(s.Host(), path.Join(s.Home(), keyStore), keyStorePassword)
+		err = k.WriteKeystore(s.Host(), keyStore, keyStorePassword)
 	}
 	return
+}
+
+func genkeypair() (cert *x509.Certificate, key *memguard.Enclave, err error) {
+	serial, err := rand.Prime(rand.Reader, 64)
+	if err != nil {
+		return
+	}
+	template := &x509.Certificate{
+		SerialNumber: serial,
+		Subject: pkix.Name{
+			CommonName: "ssokey",
+		},
+		NotBefore:             time.Now().Add(-60 * time.Second),
+		NotAfter:              time.Now().AddDate(10, 0, 0).Truncate(24 * time.Hour),
+		IsCA:                  true,
+		BasicConstraintsValid: true,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+		MaxPathLen:            -1,
+	}
+
+	privateKeyPEM, err := config.NewPrivateKey("rsa")
+	if err != nil {
+		return
+	}
+
+	return config.CreateCertificateAndKey(template, template, privateKeyPEM, nil)
 }
 
 func (s *SSOAgents) Command() (args, env []string, home string) {
